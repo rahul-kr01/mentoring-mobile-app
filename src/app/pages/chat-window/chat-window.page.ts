d6c5f8ecc8fa04df7836c0661ee895c33cc4001b
Vishwanath <vishwab@nfcsolutionsusa.com>
2024-10-24 14:54:27 +0530

    Message request api integration

--- src/app/pages/chat-window/chat-window.page.ts
+++ src/app/pages/chat-window/chat-window.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { urlConstants } from 'src/app/core/constants/urlConstants';
 import { HttpService, ToastService } from 'src/app/core/services';
+import { CHAT_MESSAGES } from '../../core/constants/chatConstants';
 
 @Component({
   selector: 'app-chat-window',
@@ -22,7 +25,9 @@ export class ChatWindowPage implements OnInit {
   constructor(
     private httpService : HttpService,
     private routerParams : ActivatedRoute,
-    private toast : ToastService
+    private toast : ToastService,
+    private alert : AlertController,
+    private translate : TranslateService
   ) { 
     routerParams.params.subscribe(parameters =>{
       console.log(parameters,"parameters");
@@ -42,7 +47,6 @@ export class ChatWindowPage implements OnInit {
       }
     }
     this.httpService.post(payload).then(resp =>{
-      console.log(resp,"resp");
       this.info = resp?.result;
       if(resp?.result?.status && resp?.result?.status == 'REQUESTED'){
         this.isRequested = resp?.result?.status;
@@ -50,20 +54,14 @@ export class ChatWindowPage implements OnInit {
       }else{
         this.info.status =  'PENDING';
       }
-      this.messages= {
-        'PENDING':{
-            title: `FIRST_MSG_REQ_INFO_TITLE`,
-            subText:"FIRST_MSG_REQ_INFO_SUBTITLE"
-        },
-        'REQUESTED':{
-          title: `MULTIPLE_MSG_REQ_TITLE`,
-          subText:`MULTIPLE_MSG_REQ_SUBTITLE`
-      }}
-    console.log(this.info.user_details?.name,"resp info");
+      if(this.info.created_by === this.info.user_id){
+        this.messages= CHAT_MESSAGES.INITIATOR
+      }else{
+        this.messages= CHAT_MESSAGES.RECEIVER
+      }
     })
   }
   sendRequest(){
-    console.log(this.isRequested,'this.isRequested');
     if(this.isRequested == 'REQUESTED'){
       this.toast.showToast('MULTIPLE_MESSAGE_REQ','danger')
       return;
@@ -77,6 +75,8 @@ export class ChatWindowPage implements OnInit {
     }
     this.httpService.post(payload).then(resp =>{
       console.log(resp,"resp");
+      this.info.status = "REQUESTED"
+      this.getConnectionInfo();
     })
   }
   acceptRequest(){
@@ -88,7 +88,38 @@ export class ChatWindowPage implements OnInit {
     }
     this.httpService.post(payload).then(resp =>{
       console.log(resp,"resp");
+      this.info.status = "ACCEPTED"
     })
+  }
+
+  async rejectConfirmation(){
+    let texts: any;
+        this.translate
+          .get(['MESSAGE_REQ_REJECT', 'REJECT','CANCEL'])
+          .subscribe((text) => {
+            texts = text;
+          });
+    const alert = await this.alert.create({
+      header: texts['REJECT'] +'?',
+      message: texts['MESSAGE_REQ_REJECT'],
+      buttons: [
+        {
+          text: texts['REJECT'] ,
+          role:'cancel',
+          handler: () => {
+           this.rejectRequest();
+          }
+        },
+        {
+          text: texts['CANCEL'] ,
+          handler: () => {
+          }
+        },
+       
+      ]
+    });
+
+    await alert.present();
   }
   rejectRequest(){
     const payload ={
@@ -98,6 +129,8 @@ export class ChatWindowPage implements OnInit {
       }
     }
     this.httpService.post(payload).then(resp =>{
+      this.isRequested ='REJECTED';
+      this.toast.showToast('REJECTED_MESSAGE_REQ','danger')
     })
   }
 }
