fbdc54d7c13cab4bdc6cfb385310711dc58d674c
Kiran Haridas <kiranharidas@apples-mbp.lan>
2024-11-13 11:28:25 +0530

    environment file changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
 	recaptchaSiteKey: window['env' as any]['recaptchaSiteKey' as any] ,
 	recaptchaSiteKeyOld: window['env' as any]['recaptchaSiteKeyOld'] ,
 	restictedPages: window['env' as any]['restictedPages'],
-	unauthorizedRedirectUrl: window['env' as any]['unauthorizedRedirectUrl' as any] ,
+	unauthorizedRedirectUrl: window['env' as any]['unauthorizedRedirectUrl' as string] ,
 	supportEmail: window['env' as any]['supportEmail' as any] ,
 	isAuthBypassed: window['env' as any]['isAuthBypassed'] ,
 	password: window['env' as any]['password'] 
