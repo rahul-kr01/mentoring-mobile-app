530493691e90141fc76c75978e5cf72b3ff91fc1
Rahul K R <rahul@tunerlabs.com>
2024-12-06 15:02:49 +0530

    unwanted files removed

--- src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  name: 'dev environment',
-  staging: false,
-  dev: true,
-  baseUrl: 'https://elevate-apis.shikshalokam.org',
-  sqliteDBName: 'mentoring.db',
-};
--- src/environments/environment.staging.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  name: 'staging environment',
-  staging: true,
-  dev: false,
-  sqliteDBName: 'mentoring.db',
-};
